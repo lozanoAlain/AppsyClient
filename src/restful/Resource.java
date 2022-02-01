170efd5e4ff5034afc8fcb1a032c6b213301d0ef
lozanoAlain <83534707+lozanoAlain@users.noreply.github.com>
2022-02-01 17:35:29 +0100

    Update Resource.java

--- src/restful/Resource.java
+++ src/restful/Resource.java
@@ -26,7 +26,6 @@ public class Resource {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:35293/AppsyServer/webresources";
 
     public Resource() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
