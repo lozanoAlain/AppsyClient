d3322c10455c1d7fcc0b5bd68af12560511c9dab
lozanoAlain <83534707+lozanoAlain@users.noreply.github.com>
2022-02-01 17:31:06 +0100

    Update Appointment.java

--- src/restful/Appointment.java
+++ src/restful/Appointment.java
@@ -26,7 +26,6 @@ public class Appointment {
 
     private WebTarget webTarget;
     private Client client;
-    private static final String BASE_URI = "http://localhost:35293/AppsyServer/webresources";
 
     public Appointment() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
